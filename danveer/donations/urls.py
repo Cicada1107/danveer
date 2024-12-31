4fa49a564955f6bbc7a1e500eda8fcbe3ce00fb8
Cicada1107 <arijitdubey2018@gmail.com>
2024-12-31 20:22:42 +0530

    main.html ready, designed a placeholder homepage. refer 'reference.txt' for structure

--- danveer/donations/urls.py
+++ danveer/donations/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),  # Home page
+    path('register/', views.register, name='register'),  # Registration page
+    path('login/', views.login, name='login'),  # Login page
+    path('profile/', views.profile, name='profile'),  # Profile page
+    path('donation/', views.donation, name='donation'),  # Donation page
+    path('confirm/', views.confirm, name='confirm'),  # Confirm page
+    path('request-donation/', views.request_donation, name='request_donation'),  # Request donation page
+    path('explore/', views.explore, name='explore'),  # Explore page
+    path('admin/', views.admin, name='admin'),  # Admin page
 ]
--- danveer/donations/views.py
+++ danveer/donations/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Home Page
+def home(request):
+    return render(request, 'home.html')
+
+# Registration Page
+def register(request):
+    return render(request, 'registration.html')
+
+# Login Page
+def login(request):
+    return render(request, 'login.html')
+
+# Profile Page
+def profile(request):
+    return render(request, 'profile.html')
+
+# Donation Page
+def donation(request):
+    return render(request, 'donation.html')
+
+# Confirm Donation Page
+def confirm(request):
+    return render(request, 'confirm.html')
+
+# Request Donation Page
+def request_donation(request):
+    return render(request, 'request_donation.html')
+
+# Explore Page
+def explore(request):
+    return render(request, 'explore.html')
+
+# Admin Page
+def admin(request):
+    return render(request, 'admin.html')
